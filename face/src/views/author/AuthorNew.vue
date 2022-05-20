e08c126c8239b4dbd9996e28e2a948503897ae6b
albert-flex <albert_flex@yeah.net>
2022-05-20 21:35:31 +0800

    author view

--- face/src/views/author/AuthorNew.vue
+++ face/src/views/author/AuthorNew.vue
@@ -1,6 +1,11 @@
 <template>
   <main>
-    <a-form :label-col="labelCol" :label-wrap="wrapperCol" :model="formData" class="newAuthor">
+    <a-form
+      :label-col="labelCol"
+      :label-wrap="wrapperCol"
+      :model="formData"
+      class="newAuthor"
+    >
       <h2 style="text-align: center">注册作者信息</h2>
       <a-form-item label="笔名">
         <a-input
@@ -20,12 +25,6 @@
       <a-form-item label="验证码">
         <a-input v-model:value="formData.captcha" allow-clear></a-input>
       </a-form-item>
-      <a-form-item label="密码">
-        <a-input-password
-          v-model:value="formData.password"
-          placeholder="input password"
-        />
-      </a-form-item>
       <a-form-item :wrapper-col="{ span: 14, offset: 10 }">
         <a-button type="primary">注册</a-button>
       </a-form-item>
@@ -36,14 +35,32 @@
 <script setup>
 import { ref } from "@vue/reactivity";
 import { UserOutlined, InfoCircleOutlined } from "@ant-design/icons-vue";
-
+import { RegisterAuthor } from "../../ports/author.js";
+import { loadAccess } from "../../config/stores.js";
+import { useRouter } from "vue-router";
 const labelCol = { style: { width: "150px" } };
 const wrapperCol = { span: 10 };
 const formData = ref({
-  userName: "",
-  email:"",
-  captcha:"",
+  name: "",
+  email: "",
+  captcha: "",
 });
+const router = useRouter();
+
+function Register() {
+  const access = loadAccess();
+  if (!access.logined()) {
+    alert("未登录，将前往登录页");
+    router.push({ name: "login" });
+  }
+  RegisterAuthor(access.access_token,formData,(data)=>{
+    if(!data.success){
+      alert(data.error);
+    }else{
+      alert("注册作者成功!");
+    }
+  });
+}
 </script>
 
 <style scoped>
@@ -54,7 +71,7 @@ main {
   margin-right: 50px;
 }
 
-.newAuthor{
+.newAuthor {
   padding: 10px;
   padding-right: 20px;
   border-radius: 10px;
